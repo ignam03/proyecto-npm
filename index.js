a6dca75a9e9831a1f7341c7126e21979814f9582
Ignacio M <carlos@openix.com.ar>
2024-02-10 11:00:37 -0300

    index subido

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('Hola mundo');
+const express = require('express');
+
+const app = express();
+
+app.listen (3000, () => console.log('Server on port 3000'));
+
+let a = 5;
+
+console.log(a);
